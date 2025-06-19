c5e88b6d3ba0fdaad173a39db65bf4d8286c8f82
Rafael Barbosa <rafael.barbosa51@gmail.com>
2025-06-19 12:03:56 -0400

    Init

---
+++ index.js
@@ -0,0 +1,31 @@
+/** This function adds numbers a + b 
+ * @param {number} a
+ * @param {number} b
+*/
+export const add = (a, b) => {
+    return a + b;
+}
+
+/** This function subtract numbers a - b 
+ * @param {number} a
+ * @param {number} b
+*/
+export const subtract = (a, b) => {
+    return a - b;
+}
+
+/** This function multiplies numbers a * b 
+ * @param {number} a
+ * @param {number} b
+*/
+export const multiply = (a, b) => {
+    return a * b;
+}
+
+/** This function divides numbers a / b 
+ * @param {number} a
+ * @param {number} b
+*/
+export const divide = (a, b) => {
+    return a + b
+}
